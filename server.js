76f852c71ad564a02931e74805bbbe0437aa52b2
Evisari Riyanto <evisari111012@gmail.com>
2020-10-19 19:43:49 -0700

    added more

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const morgan = require("morgan");
+const movies = require("./movies-data.json");
 
 const app = express();
 const PORT = 8000;
 
 app.use(morgan("common"));
 
-app.use((req, res) => {
-  res.send("hello from moviedex");
+app.get("/movie", (req, res) => {
+  const { genre, country, avg_vote } = req.query;
+
+  let results;
+
+  if (genre) {
+    results = movies.filter((movie) =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+
+  if (country) {
+    results = movies.filter((movie) =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+
+  res.json(results);
 });
 
 app.listen(PORT, () => {
